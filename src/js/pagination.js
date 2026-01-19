5dedb0113130f80b8d30193830667fe0cf68d0f0
almaz73 <almaz_73@mail.ru>
2026-01-19 09:57:55 +0700

    pagination fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,37 +1,32 @@
-
 /** Очень тонко  настроенная работает Пагинации*/
 let currentPage = 1
+
 export function preparePager(pages) {
     const pager = document.querySelector('#pager')
-    if(!pager) return false
+    if (!pager) return false
     const urlParams = new URLSearchParams(window.location.search);
     let brand = urlParams.get('brand')
     let pagerText = ''
 
     currentPage = +urlParams.get('page') || 1;
-    if (brand) brand = `?brand=${brand.slice(0, -1)}/`
-    if (brand && brand.includes('/')) {
-        brand = brand.split('/')[0]
-        currentPage = +location.search.split('/')[1].split('=')[1]
-        currentPage = currentPage||1
-    }
-    const  delta = currentPage>3?currentPage-3:0
+    const delta = currentPage > 3 ? currentPage - 3 : 0
     brand = brand ? brand + '/?' : ''
 
-    if (delta > 0) pagerText += `<a href="/cars/${brand}"> 1</a> <span> | </span> ... <span> | </span>`
+    if (delta > 0) pagerText += `<a href=${location.href.slice(0, location.href.lastIndexOf('&page'))}> 1</a> <span> | </span> ... <span> | </span>`
 
     for (let N = 1 + delta; N < pages; N++) {
         if (N - delta > 5) break
-        pagerText += `<a ${currentPage === N ? 'class="active"' : ''} href="/cars/${brand}?page=${N}">${N}</a><span> | </span>`
+        pagerText += `<a ${currentPage === N ? 'class="active"' : ''} href=${location.href.slice(0, location.href.lastIndexOf('&page'))}&page=${N}>${N}</a><span> | </span>`
     }
-    if (pages > 5) pagerText += ` ... <span> | </span>  <a href="${brand}?page=${pages}">${pages}</a><span> | </span> `
-    if (pages !== currentPage && window.screen.width>500) pagerText += `<a href="javascript:nextPage('${brand}')"> След.</a>`
+    if (pages > 5) pagerText += ` ... <span> | </span>  <a href=${location.href.slice(0, location.href.lastIndexOf('&page'))}&page=${pages}>${pages}</a><span> | </span> `
+    if (pages !== currentPage && window.screen.width > 500) pagerText += `<a href="javascript:nextPage('${currentPage}')"> След.</a>`
     pager.innerHTML = pagerText
 }
 
-window.nextPage = (brand)=>{
-    if (window.location.search.includes('/')) location.href = `/cars/${brand}page=` + (currentPage + 1)
-    else location.href = `/cars/${brand}?page=` + (currentPage + 1)
+window.nextPage = (currentPage) => {
+    let newPage = (`&page=` + (+currentPage + 1))
+    if (location.href.includes('&page=')) location.href = location.href.slice(0, location.href.lastIndexOf('&page')) + newPage
+    else location.href = location.href + newPage
 }
 
 
