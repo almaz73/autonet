7b7cb611331b44d95af95c0431812ecabbd89509
almaz73 <almaz_73@mail.ru>
2026-01-19 11:27:19 +0700

    upd

--- docs/assets/cars-90yVoWpZ.js
+++ docs/assets/cars-90yVoWpZ.js
@@ -1,4 +1,4 @@
-import"./main-CQbA20Ps.js";/* empty css              */import"./brands-icons-DB-M3PCQ.js";import"./filter-controls-BeNwMr_Q.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-CO_6yeV6.js";import"./favoriteCars-XAsSA611.js";import"./compareCars-C42htp3j.js";const e=`<div  class="filter-white">
+import"./main-CQbA20Ps.js";/* empty css              */import"./brands-icons-puF5Iaue.js";import"./filter-controls-CFSCoSwg.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-DAQO3WFk.js";import"./favoriteCars-r2BI97Ws.js";import"./compareCars-DhB5upfo.js";const e=`<div  class="filter-white">
 <div class="filter-white-back filter-fields">
         <div class="frame-filter__item">
             <comb data-placeholder='Марка' />
