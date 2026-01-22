a2c5806468cc60d1a8c2e3520aa9322c236b666b
almaz73 <almaz_73@mail.ru>
2026-01-22 13:22:40 +0700

    upd

--- docs/assets/cars-DXHLlC-a.js
+++ docs/assets/cars-DXHLlC-a.js
@@ -1,4 +1,4 @@
-import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-ds3S4EWx.js";import{g as s}from"./global-func-BLfbnEIy.js";import"./filter-controls-DeKuDqtR.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-DAQO3WFk.js";import"./favoriteCars-D3BlehqI.js";import"./compareCars-aUEkwXup.js";const l=`<div  class="filter-white">
+import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-Dudce8X_.js";import{g as s}from"./global-func-BLfbnEIy.js";import"./filter-controls-BNbu41Xi.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-DAQO3WFk.js";import"./favoriteCars-jr1hwINq.js";import"./compareCars-aUEkwXup.js";const l=`<div  class="filter-white">
 <div class="filter-white-back filter-fields">
         <div class="frame-filter__item">
             <comb data-placeholder='Марка' />
@@ -16,7 +16,8 @@ import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-ds3
         <div style="flex-grow: 1">
             <div class="big-combo control-flex">
                 <span style="width: 60px">Год от:</span>
-                <input class="big-comb__input" style="width: 40px" value="1997" onclick="this.select()">
+                <input class="big-comb__input" style="width: 40px" 
+                onchange="input_chamged('yearReleasedFrom', this.value)" onclick="this.select()">
             </div>
         </div>
 
@@ -31,14 +32,14 @@ import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-ds3
             <comb data-placeholder='Цвет'/>
         </div>
 
-        <div>
+        <div class="no_advanced">
             <div class="big-combo control-flex">
                 <span style="width: 80px">Цена от: </span>
                 <input class="big-comb__input" style="width: 70px" value="6882400" onclick="this.select()">
             </div>
         </div>
 
-        <div class="no_advanced">
+        <div>
             <div class="big-combo control-flex">
                 <span style="width: 80px">Цена до: </span>
                 <input class="big-comb__input" style="width: 70px" value="6882400" onclick="this.select()">
@@ -98,10 +99,10 @@ import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-ds3
         <a href="javascript:void(0)" onclick="clearFilter()" class="frame-filter__controls-reset">Сбросить</a>
 
         <button class="frame-filter__submit" id="set_filter" onclick="goToCars()">
-            Показать&nbsp;<span class="number"></span>
+            Найдено&nbsp;<span class="number"></span>
         </button>
     </div>
-</div>`,t=`<div class="car_vitrina">
+</div>`,e=`<div class="car_vitrina">
         <h2 id="vitrina_name">Автомобили</h2>
 
         <a href="/cars#vitrina_name" class="page__link cars_link" style="display: none">
@@ -127,4 +128,4 @@ import"./main-D6G3P3ZD.js";/* empty css              */import"./brands-icons-ds3
                 <img src="/src/svg/arrow-down.svg" alt="arrow" loading="lazy" width='10' height='18'>
             </span>
         </span>
-    </div><cards class='cards dot4' data-col='dot4'></cards>`;let a=s("brand");const i=s("id");i||(document.querySelector("#filter_cars").innerHTML=l);i||(document.querySelector("#car-vitrina").innerHTML=t);a&&!i&&(a.includes("/")&&(a=a.split("/")[0]),document.querySelector("#path").innerHTML=a.toUpperCase());i&&console.log("66445",121212);
+    </div><cards class='cards dot4' data-col='dot4'></cards>`;let a=s("brand");const i=s("id");i||(document.querySelector("#filter_cars").innerHTML=l);i||(document.querySelector("#car-vitrina").innerHTML=e);a&&!i&&(a.includes("/")&&(a=a.split("/")[0]),document.querySelector("#path").innerHTML=a.toUpperCase());i&&console.log("66445",121212);
