4b2bf625eec282d506d46839c3a120cbf6dd3a56
almaz73 <almaz_73@mail.ru>
2026-02-03 07:21:23 +0700

    upd

---
+++ docs/assets/podbor-CFnw005d.js
@@ -0,0 +1,43 @@
+import{i as d}from"./main-X3fqOo_L.js";/* empty css                *//* empty css                     *//* empty css                *//* empty css                *//* empty css                   *//* empty css                          */import"./swiper-starter-Bjhk_3sq.js";document.addEventListener("DOMContentLoaded",()=>{let i=document.querySelector("#right_panel_content"),o=document.querySelector(".apply_bid"),a=`<div>
+    <div class="div">
+        <h2>Заявка на <span style="color: var(--color-red)">автоподбор</span></h2>
+    </div>
+
+    <div class="div promo-form">
+        <div class="formBlock">
+            <div class="form__modal--group">
+                <div class="form__group">
+                    <input size="25" placeholder="Имя *">
+                </div>
+            </div>
+            
+            <div class="form__modal--group">
+                <div class="form__group">
+                    <input size="25" placeholder="Телефон *">
+                </div>
+            </div>
+                  
+            <div class="form__modal--group">
+                <div class="form__group">
+                    <input size="25" placeholder="E-mail">
+                </div>
+            </div>
+
+            <div class="form__modal--group">
+              <div class="capctha-div"></div>
+            </div>
+
+            <button class="page__btn page__btn--current">
+                <span> Отправить заявку </span>
+            </button>
+
+            <div class="modal__personal">
+                <input type="checkbox">
+                <label> Нажав кнопку «Отправить заявку» я даю согласие на обработку
+                    <a href="/privacy-policy/" target="_blank">персональных данных</a>
+                </label>
+            </div>
+        </div>
+    </div>
+
+</div>`;o.addEventListener("click",()=>{openRightPanel(),i.innerHTML=a,d()})});
