dd5619c36419dffdf72628d00cd5be2e16cce26b
almaz73 <almaz_73@mail.ru>
2026-02-03 20:03:24 +0700

    upd

--- docs/assets/cars-D70uCy7-.js
+++ docs/assets/cars-D70uCy7-.js
@@ -1,4 +1,4 @@
-import"./main-DDG4M_xQ.js";/* empty css                        */import"./brands-icons-BBj_tEi3.js";import{g as h}from"./global-func-2OSMyxPn.js";import{a as y}from"./apibase-BsWjPdUp.js";import"./filter-controls-9gntpz2g.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-Bjhk_3sq.js";import"./favoriteCars-B6JjzAIO.js";import"./compareCars-CO-5H6Pg.js";const _=`<div  class="filter-white">
+import"./main-DDG4M_xQ.js";/* empty css                        */import"./brands-icons-C4eC6kDg.js";import{g as h}from"./global-func-DySQm8r7.js";import{a as y}from"./apibase-BsWjPdUp.js";import"./filter-controls-DP7kPRz2.js";/* empty css                          */import"./swiper_buy-CDu4HQCt.js";import"./swiper-starter-Bjhk_3sq.js";import"./favoriteCars-B6JjzAIO.js";import"./compareCars-DKwOBjD7.js";const _=`<div  class="filter-white">
 <div class="filter-white-back filter-fields">
         <div class="frame-filter__item"
              onmouseenter="onCard(this)">
