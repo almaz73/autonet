dc809aa439ecce548ae06627eccc6691f49ddfc0
almaz73 <almaz_73@mail.ru>
2026-02-04 12:29:48 +0700

    shinny

---
+++ src/js/shinny.js
@@ -0,0 +1,35 @@
+import {initCaptcha} from "@/js/captcha.js";
+import {checkFormFields, formattingPhone} from "@/js/global-func.js";
+import {franshiza} from "@/js/global-constants.js";
+
+window.formattingPhone = formattingPhone
+document.addEventListener('DOMContentLoaded', () => {
+
+    let form1 = document.querySelector('.formBlock.v1')
+    form1.innerHTML = franshiza
+    form1.querySelector('.formBottom').style.display = 'none'
+    form1.querySelector('.title').innerHTML = `Записаться на шиномонтаж. Диагностика колес <span style="color:red">бесплатно</span>`
+
+    initCaptcha()
+})
+
+window.callMe = function (self) {
+    let ver = self.parentNode.parentNode.parentNode.parentNode.classList[1]
+    let capcthadiv = document.querySelector(`.${ver} .capctha-div`)
+    let name = document.querySelector(`.${ver} [name="name"]`)
+    let phone = document.querySelector(`.${ver} [name="phone"]`)
+    let city = document.querySelector(`.${ver} [name="city"]`)
+    let checkbox = document.querySelector(`.${ver} [type="checkbox" ]`)
+
+    if (checkFormFields([capcthadiv, name, phone, city, checkbox])) return false
+
+    let params = {
+        name: name.value,
+        dity: dity.value,
+    }
+    console.log('params', params)
+    // api_postCallToSell(params).then(res => {
+    //     if (res && res.ok) message('Заявка оптарвлена')
+    //     else message('Сервер не принял', 'error')
+    // })
+}
