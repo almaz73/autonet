b02b48f9ddbd2c4aa5640e86be6dd51a02b5e64e
almaz73 <almaz_73@mail.ru>
2026-02-10 08:39:33 +0700

    upd

--- docs/assets/cars-B7KNl8kl.js
+++ docs/assets/cars-B7KNl8kl.js
@@ -1,4 +1,4 @@
-import{g as f,a as _}from"./main-KAaFBW13.js";/* empty css                        */import"./brands-icons-SJczBycU.js";import"./filter-controls-DlpQtDIS.js";/* empty css                          */import"./swiper_buy-qJVZRekP.js";import"./swiper-starter-BitlMDKb.js";import"./favoriteCars-B6JjzAIO.js";import"./compareCars-CI0XcmVj.js";const y=`<div  class="filter-white">
+import{g as f,a as _}from"./main-CNBYApHP.js";/* empty css                        */import"./brands-icons--Cy86jmu.js";import"./filter-controls-793B6RG7.js";/* empty css                          */import"./swiper_buy-qJVZRekP.js";import"./swiper-starter-BitlMDKb.js";import"./favoriteCars-B6JjzAIO.js";import"./compareCars-Ba2_oSQF.js";const y=`<div  class="filter-white">
 <div class="filter-white-back filter-fields">
         <div class="frame-filter__item"
              onmouseenter="onCard(this)">
