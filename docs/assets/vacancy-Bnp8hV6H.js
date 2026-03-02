fcb2d0b2c8225dc8739d02ffc12129d9db872216
almaz73 <almaz_73@mail.ru>
2026-03-02 07:37:41 +0700

    upd

--- docs/assets/vacancy-Bnp8hV6H.js
+++ docs/assets/vacancy-Bnp8hV6H.js
@@ -1,4 +1,4 @@
-import{d as y,H as h,I as b,J as p,h as g}from"./main-DWFtbTbq.js";/* empty css                        *//* empty css                */function _(){let a=document.querySelector("comb"),e={Город:b};e.Город.value=localStorage.getItem("selectedCity"),e.Город.value="Казань",i(e.Город.value);let t=a.dataset.placeholder,s=e[t];if(s){let n=s.map(o=>'<div data-parent="'+t+'">'+o+"</div>");a.innerHTML=`<div class='big-combo' tabindex='1'>
+import{d as y,H as h,I as b,J as p,h as g}from"./main-1vbC9IDD.js";/* empty css                        *//* empty css                */function _(){let a=document.querySelector("comb"),e={Город:b};e.Город.value=localStorage.getItem("selectedCity"),e.Город.value="Казань",i(e.Город.value);let t=a.dataset.placeholder,s=e[t];if(s){let n=s.map(o=>'<div data-parent="'+t+'">'+o+"</div>");a.innerHTML=`<div class='big-combo' tabindex='1'>
         <span class='big-comb__selected'>
           <span class='big-comb__placeholder'>${s.value||t}</span>
           <input class='big-comb__input' type="text">
