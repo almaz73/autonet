2536d4c0a771a2c271bf9b7a926f6af1640d9ff9
almaz73 <almaz_73@mail.ru>
2026-03-05 06:10:47 +0700

    upd

--- docs/assets/cars-CnSROUBU.js
+++ docs/assets/cars-CnSROUBU.js
@@ -1,4 +1,4 @@
-import{g as f,a as _}from"./main-C0CNRGAg.js";/* empty css                        */import"./brands-icons-FY1EZV7S.js";import"./filter-controls-nzlb8fCj.js";/* empty css                          */import"./swiper_buy-Casg3Lg_.js";import"./swiper-starter-B5j2mkqB.js";const y=`<div  class="filter-white">
+import{g as f,a as _}from"./main-Q2xiCncJ.js";/* empty css                        */import"./brands-icons-HXa8GQ9d.js";import"./filter-controls-CYaib2gL.js";/* empty css                          */import"./swiper_buy-Casg3Lg_.js";import"./swiper-starter-B5j2mkqB.js";const y=`<div  class="filter-white">
 <div class="filter-white-back filter-fields">
         <div class="frame-filter__item"
              onmouseenter="onCard(this)">
