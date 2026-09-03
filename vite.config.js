94ae3279120c0db66794a9656814cbeffcbd0b12
almaz <almaz_73@mail.ru>
2026-09-03 09:30:07 +0300

    promo 660827

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ const pages = {
     promo: resolve(__dirname, './promo/index.html'),
     p660822: resolve(__dirname, './promo/660822/index.html'),
     p660823: resolve(__dirname, './promo/660823/index.html'),
-    // p660786: resolve(__dirname, './promo/660786/index.html'),
+    p660827: resolve(__dirname, './promo/660827/index.html'),
     // p660787: resolve(__dirname, './promo/660787/index.html'),
     // p660788: resolve(__dirname, './promo/660788/index.html'),
     about: resolve(__dirname, './about-the-company/index.html'),
