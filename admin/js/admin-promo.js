73f7f1a4720b4bce90ac1b2a0931ea07ab0df30e
almaz <almaz_73@mail.ru>
2026-09-07 13:16:07 +0300

    надпись на баннерах. 4. Запись и чтение админки текста надписи и стиля надписи

--- admin/js/admin-promo.js
+++ admin/js/admin-promo.js
@@ -103,7 +103,7 @@ function showPromo(result) {
 }
 
 function clearPanel() {
-    document.querySelector('#qw0').value = ''
+    document.querySelector('#qw0').innerHTML = ''
     document.querySelector('#qw1').checked = false
     document.querySelector('#qw2').value = ''
     document.querySelector('#qw3').checked = false
@@ -114,6 +114,8 @@ function clearPanel() {
     document.querySelector('#_278').alt = ''
     document.querySelector('#_585').alt = ''
     document.querySelector('#_1200').alt = ''
+    document.querySelector('#descriptionInput').innerHTML = ''
+    document.querySelector('#styles').innerHTML = ''
 }
 
 function prepareModal(id) {
@@ -157,23 +159,26 @@ window.editPromo = function (id) {
     if (id) {
         let row = datas.find(row => row.id === id)
         if (row) {
-            document.querySelector('#qw0').value = row.name
+            document.querySelector('#qw0').innerHTML = row.name
             document.querySelector('#qw1').checked = row.onMain
             document.querySelector('#qw2').value = row.priority
             document.querySelector('#qw3').checked = row.active
             document.querySelector('#qw4').value = row.code
+            document.querySelector('#descriptionInput').innerHTML = row.description
+            document.querySelector('#styles').innerHTML = row.styles
             if (row.photo278) setPhoto(row.photo278)
             if (row.photo585) setPhoto(row.photo585)
             if (row.photo1200) setPhoto(row.photo1200)
 
             if (!row.code) codGeneration()
+            if(row.styles || row.description) showOnBanner()
         }
     }
 }
 
 function getModalFields() {
     return {
-        name: document.querySelector('#qw0').value,
+        name: document.querySelector('#qw0').innerHTML,
         onMain: document.querySelector('#qw1').checked,
         priority: +document.querySelector('#qw2').value,
         active: document.querySelector('#qw3').checked,
@@ -181,6 +186,8 @@ function getModalFields() {
         photo278: document.querySelector('#_278').alt,
         photo585: document.querySelector('#_585').alt,
         photo1200: document.querySelector('#_1200').alt,
+        description: document.querySelector('#descriptionInput').innerHTML,
+        styles: document.querySelector('#styles').innerHTML,
     }
 }
 
@@ -201,7 +208,7 @@ window.savePromoModal = function (withoutClose) {
     if (!data.name) return alert('Поле "Название" обязателен для заполнения')
     data.id = dirties[0]
 
-    api_savePromo(data, val => {        
+    api_savePromo(data, val => {
         api_getPromo(showPromo)
         if (!withoutClose) {
             modal.close()
@@ -292,6 +299,40 @@ window.setSelected = function (val, self) {
     SEL.forEach(el => el.classList.remove('selected'))
     self.classList.add('selected')
     selectedField = val
+}
+
+window.addDescription = function () {
+    document.querySelector('#descriptionInput').innerHTML = `Данная акция действует на территории г.Казани, проспект Победы 212к2
+Акции не суммируются и не являются офертой. Срок действия до 30.09.2026`
+    showOnBanner()
+}
+
+window.addStyles = function () {
+    document.querySelector('#styles').innerHTML = `color: white;background: pink`
+    showOnBanner()
+}
+
+const descriptionInput = document.getElementById('descriptionInput');
+const stylesDiv = document.getElementById('styles');
+const descriptionShow1 = document.getElementById('descriptionShow1');
+const descriptionShow2 = document.getElementById('descriptionShow2');
+
+
+descriptionInput.addEventListener('input', showOnBanner);
+stylesDiv.addEventListener('input', showOnBanner);
+
+function showOnBanner() {
+    let text = descriptionInput.innerHTML
+    let styles = stylesDiv.innerHTML
+    let lines = text.split('\n')
+    let html = ''
+    lines.forEach(el => html += '<div>' + el + '</div>')
+
+    // Записываем это значение в оба дива
+    descriptionShow1.innerHTML = html;
+    descriptionShow2.innerHTML = html;
+    descriptionShow1.style.cssText=styles
+    descriptionShow2.style.cssText=styles
 }
 
 
